3ca95b39d831b0f8bb174473ea457a5146a8e4fa
rina-1 <cba.mac@icloud.com>
2020-06-29 13:51:44 +0000

    action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,15 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            'cba.you.your@gmail.com',
+    password:             ENV['GMAIL_KEY'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+   }
 
 end
